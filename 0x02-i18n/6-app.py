1058ed00175f095b29972314315d4325980e519e
Azara <yyushahu02@gmail.com>
2024-07-31 03:12:03 +0000

    implemennt timezone and user locale appropriates

---
+++ 0x02-i18n/6-app.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+"""Basic Flask App"""
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+
+
+class Config:
+    """ Configuration for Babel
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+babel = Babel(app)
+app.config.from_object(Config)
+
+
+@app.route('/')
+def index():
+    """ Index route that returns content of the index page
+    """
+    return render_template("6-index.html")
+
+
+@babel.localeselector
+def get_locale():
+    """ Determines the best language match with the predefined/supported
+        languages in the Config class
+    """
+    locale = request.args.get("locale")
+    if locale and locale in app.config["LANGUAGES"]:
+        return locale
+
+    elif g.user:
+        user_settings = g.user.get("locale")
+        if user_settings:
+            if user_settings in app.config["LANGUAGES"]:
+                return user_settings
+
+    accept_langs = request.headers.get("Accept-Language")
+    if accept_langs:
+        langs = []
+        for lang in accept_langs.split(","):
+            langs.append(lang.split(";")[0])
+        for lang in langs:
+            if lang in app.config["LANGUAGES"]:
+                return lang
+
+    return app.config["BABEL_DEFAULT_LOCALE"]
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user():
+    """ Returns a dictionary
+    """
+    login_as = request.args.get("login_as")
+    if login_as:
+        id = int(login_as)
+        return users.get(id)
+    return None
+
+
+@app.before_request
+def before_request():
+    """ Executes before any other function
+    """
+    g.user = get_user()
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
