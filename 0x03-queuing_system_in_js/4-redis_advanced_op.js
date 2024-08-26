8e97306435ae0c60fc1f9ed1df7f22d709bf89d0
Azara <yyushahu02@gmail.com>
2024-08-26 17:40:04 +0000

    use redis client to store a hash value and retrieve set hash value with hgetall and a callback

---
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,32 @@
+import { createClient } from "redis";
+
+import redis from "redis";
+
+const client = createClient();
+
+client.on('error', err => console.log('Redis client not connected to the server', err));
+
+client.on('connect', () => console.log('Redis client connected to the server'));
+
+function hashSet(hashKey, key, value) {
+  client.hset(hashKey, key, value, redis.print);
+};
+
+function hashGetAll(hashKey) {
+  client.hgetall(hashKey, (value, err) => {
+    if (value) {
+      console.log(value);
+    } else {
+      console.error(err);
+    }
+  });
+};
+
+hashSet('HolbertonSchools', 'Portland', '50');
+hashSet('HolbertonSchools', 'Seattle', '80');
+hashSet('HolbertonSchools', 'New York', '20');
+hashSet('HolbertonSchools', 'Bogota', '20');
+hashSet('HolbertonSchools', 'Cali', '40');
+hashSet('HolbertonSchools', 'Paris', '2');
+
+hashGetAll('HolbertonSchools');
