b7d96c171b732b04fb016e5ba2c5385c847b09c4
Azara <yyushahu02@gmail.com>
2024-08-26 17:21:34 +0000

    modify function displaySchoolName to use ES6 async/await

--- 0x03-queuing_system_in_js/2-redis_op_async.js
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -1,8 +1,10 @@
 import { createClient } from "redis";
+import { promisify } from "util";
 
 import redis from "redis";
 
 const client = createClient();
+const getAysnc = promisify(client.get).bind(client);
 
 client.on('error', err => console.log('Redis client not connected to the server', err));
 
@@ -12,14 +14,13 @@ function setNewSchool(schoolName, value) {
   client.set(schoolName, value, redis.print);
 };
 
-function displaySchoolValue(schoolName) {
-  client.get(schoolName, (value, err) => {
-    if (value) {
-      console.log(value);
-    } else {
-      console.error(err);
-    }
-  });
+async function displaySchoolValue(schoolName) {
+  try {
+    const value = await getAysnc(schoolName);
+    console.log(value);
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 displaySchoolValue('Holberton');
