b94c7333447ba322c90611d8cf228fce664c49fe
Azara <yyushahu02@gmail.com>
2024-08-26 17:05:49 +0000

    perform basic set and get ops using node and redis

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,27 @@
+import { createClient } from "redis";
+
+import redis from "redis";
+
+const client = createClient();
+
+client.on('error', err => console.log('Redis client not connected to the server', err));
+
+client.on('connect', () => console.log('Redis client connected to the server'));
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, redis.print);
+};
+
+function displaySchoolValue(schoolName) {
+  client.get(schoolName, (value, err) => {
+    if (value) {
+      console.log(value);
+    } else {
+      console.error(err);
+    };
+  });
+};
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
