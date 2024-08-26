3b30f0499b0b8e53e0b7c41aba0f841001e65a99
Azara <yyushahu02@gmail.com>
2024-08-26 19:11:27 +0000

    use client.duplicate for the subscriber

--- 0x03-queuing_system_in_js/5-subscriber.js
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -1,19 +1,19 @@
 import { createClient } from "redis";
 
 const client = createClient();
-// const subscriber = client.duplicate();
+const subscriber = client.duplicate();
 
 client.on('error', err => console.log('Redis client not connected to the server:', err));
 
 client.on('connect', () => console.log('Redis client connected to the server'));
 
-client.subscribe('holberton school channel');
+subscriber.subscribe('holberton school channel');
 
-client.on('message', (channel, message) => {
+subscriber.on('message', (channel, message) => {
   if (message === 'KILL_SERVER') {
     console.log(message);
-    client.unsubscribe('holberton school channel');
-    client.quit();
+    subscriber.unsubscribe('holberton school channel');
+    subscriber.quit();
   } else {
     console.log(message);
   }
