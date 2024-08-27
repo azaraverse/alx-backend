ab79e7fa8f516766c2965be36e402bed80c547ac
Azara <yyushahu02@gmail.com>
2024-08-27 13:32:26 +0000

    implement storing stock in redis and product detail...

--- 0x03-queuing_system_in_js/9-stock.js
+++ 0x03-queuing_system_in_js/9-stock.js
@@ -1,3 +1,10 @@
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+const client = createClient();
+
+const getAsync = promisify(client.get).bind(client);
+
 const express = require('express');
 
 const app = express();
@@ -5,21 +12,21 @@ const port = 1245;
 
 const listProducts = [
   {
-    id: 1, name: 'Suitcase 250', price: 50, stock: 4,
+    itemId: 1, itemName: 'Suitcase 250', price: 50, initialAvailableQuantity: 4,
   },
   {
-    id: 2, name: 'Suitcase 450', price: 100, stock: 10,
+    itemId: 2, itemName: 'Suitcase 450', price: 100, initialAvailableQuantity: 10,
   },
   {
-    id: 3, name: 'Suitcase 650', price: 350, stock: 2,
+    itemId: 3, itemName: 'Suitcase 650', price: 350, initialAvailableQuantity: 2,
   },
   {
-    id: 4, name: 'Suitcase 1050', price: 550, stock: 5,
+    itemId: 4, itemName: 'Suitcase 1050', price: 550, initialAvailableQuantity: 5,
   },
 ];
 
-function getItemById(id) {
-  return listProducts.find((product) => product.id === id);
+function getItemById(itemId) {
+  return listProducts.find((product) => product.itemId === itemId);
 }
 
 app.get('/list_products', (request, response) => {
@@ -32,6 +39,38 @@ app.get('/list_products', (request, response) => {
     }
   }
   response.json(array);
+});
+
+
+function reserveStockById(itemId, stock) {
+  const item = getItemById(itemId);
+  client.set(`item.${item.itemId}`, stock);
+};
+
+async function getCurrentReservedStockById(itemId) {
+  try {
+    const value = await getAsync(`item.${itemId}`);
+    return value;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+app.get('/list_products/:itemId(\\d+)', async (request, response) => {
+  const { itemId } = request.params;
+  const item = getItemById(parseInt(itemId));
+  if (!item) {
+    return response.json({ "status": "Product not found" });
+  }
+  const reservedStock = await getCurrentReservedStockById(parseInt(itemId));
+  const res = {
+    itemId: item.itemId,
+    itemName: item.itemName,
+    price: item.price,
+    initialAvailableQuantity: item.initialAvailableQuantity,
+    currentQuantity: reservedStock || item.initialAvailableQuantity
+  };
+  response.json(res);
 });
 
 app.listen(port);
